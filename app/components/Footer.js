aa6b0b2189305ea915bc24ea35796b3925198971
frahim arif <frahim9900@gmail.com>
2026-06-25 11:25:01 +0530

    chang

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 // components/Footer.js
 export default function Footer() {
   return (
-    <footer className="bg-white border-t mt-12">
+    <footer className="bg-pink border-t mt-12">
       <div className="max-w-6xl mx-auto px-4 py-6 flex flex-col sm:flex-row justify-between items-center gap-4">
         <div>© {new Date().getFullYear()} WebcoreCubeTech — All rights reserved</div>
         <div className="flex gap-4 text-sm">
